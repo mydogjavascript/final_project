51d43a1e69fdc46cb6b79aa3dca2cd623a87c5be
mydogmandy <62800249+mydogmandy@users.noreply.github.com>
2020-09-05 19:01:47 -0500

    working model

--- Dashboard_testing/app.py
+++ Dashboard_testing/app.py
@@ -1,30 +1,39 @@
-import requests
-from flask import Flask, request, jsonify, render_template
+from flask import (Flask, render_template, request)
+import pandas as pd
 import numpy as np
 import joblib
-import pandas as pd
+import pickle
 
 app = Flask(__name__)
-model = joblib.load(open('crash_predictor.joblib', 'rb'))
+model = joblib.load(open("crash_predictor2.pkl", "rb"))
+# prediction function
 
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index_test.html')
 
 
 @app.route('/predict', methods=['POST'])
-def predict():
-
-    int_features = [int(x) for x in request.form.values()]
-    print("int_features", int_features)
-    final_features = [np.array(int_features)]
-    print("final_features", final_features)
-    prediction = model.predict(final_features)
-    print("prediction", prediction)
-
-    return render_template('index.html', prediction_text="hello")
+def ValuePredictor(to_predict_list):
+    to_predict = np.array(to_predict_list).reshape(1, 8)
+    result = model.predict(to_predict)
+    return result[0]
 
 
-if __name__ == "__main__":
+@app.route('/result', methods=['POST'])
+def result():
+    if request.method == 'POST':
+        to_predict_list = request.form.to_dict()
+        to_predict_list = list(to_predict_list.values())
+        to_predict_list = list(map(int, to_predict_list))
+        result = ValuePredictor(to_predict_list)
+        if int(result) == 1:
+            prediction = 'Too bad, YOU DEAD'
+        else:
+            prediction = 'You Survived!'
+        return render_template("index_test.html", prediction=prediction)
+
+
+if __name__ == '__main__':
     app.run(debug=True)
